1c56d230d5d8b3ece678c67d557ef6ce7262d47b
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-01-02 23:37:33 -0300

    database in localhost

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const app = express();
 
+moogose.connect("mongodb://localhost/todo_express", {
+    usedNewUrlParser: true,
+    usedUnifiedTopology: true,
+});
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+
 app.listen(3000, () => console.log("Server lendo na porta 3000"))
