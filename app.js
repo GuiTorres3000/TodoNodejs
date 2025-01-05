444b4de42543cc1dc77432efdb53792d45e30797
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-01-04 23:33:36 -0300

    View

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use(express.urlencoded({ extended: true}))
 app.use(express.static("public"));
 app.set("view engine", "ejs");
  
-
+app.use(require("./routes/index"))
 
 app.listen(3000, () => console.log("Server lendo na porta 3000"))
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,4 @@ const router = require("express").Router()
 router.get("/", (req, res) => {
     res.send("Página Inicial!")
 })
-
 module.exports = router;
